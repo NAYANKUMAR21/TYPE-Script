8af8635ff94bd7f93eda2bbbeb888d3abef18007
Nayan Kumar <naayaankumar@gmail.com>
2023-03-17 17:29:54 +0530

    adde redis to pract

--- Pract1/index.js
+++ Pract1/index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
-
+const Redis = require('ioredis');
+const redis = new Redis();
+let jwt = require('jsonwebtoken');
 app.get('/', (req, res) => {
+  const token = jwt.sign({ id: 'NayanKumar', roll: '1234' }, 'XYZ');
+  redis.set('token', token);
   return res.send({ roll: '12345' });
 });
-
+app.get('/show', async (req, res) => {
+  const savedToken = await redis.get('token');
+  console.log(savedToken);
+  const verify = jwt.verify(savedToken, 'XYZ');
+  return res.status(200).send({ savedToken, verify });
+});
 app.listen(8080, () => {
   console.log('http://localhost:8080');
 });
