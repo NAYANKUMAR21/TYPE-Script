526ff4c520dc1118f2a639d4f882dfbcd4b8cbf4
Nayan Kumar <naayaankumar@gmail.com>
2023-03-18 02:49:56 +0530

    scla and kotlin branch

---
+++ Auth/Auth/AuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthApp.swift
+//  Auth
+//
+//  Created by Nayan Kumar on 18/03/23.
+//
+
+import SwiftUI
+
+@main
+struct AuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
