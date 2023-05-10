860edef6fbf18963f14189941f85a7c4cda9626e
Nayan Kumar <naayaankumar@gmail.com>
2023-05-10 19:35:49 +0530

    swift app

--- Auth/Auth/ContentView.swift
+++ Auth/Auth/ContentView.swift
@@ -13,7 +13,12 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Hello, world!");
+            Text("Hello, world!");
+            Text("Hello, world!");
+            Text("Hello, world!");
+            Text("Hello, world!");
+            Text("Hello, world!");
         }
         .padding()
     }
